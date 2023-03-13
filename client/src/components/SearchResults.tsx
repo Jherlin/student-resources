3c028b272b6088e27f07838fff96a344f1d1d8c3
Jherlin <jherlingg@gmail.com>
2023-03-13 16:33:19 -0400

    Add white background to images and update media queries for dynamic changes to the container

--- client/src/components/SearchResults.tsx
+++ client/src/components/SearchResults.tsx
@@ -9,9 +9,11 @@ const SearchResults = ( { searchResults }: SearchResultsProps) => {
             <div className="media-content">
               <h2><a href={item.url} target="_blank" rel="noreferrer">{item.title}</a></h2>
               <a className="website-link" href={item.url} target="_blank" rel="noreferrer">{item.url}</a>
-              <p>{item.description.slice(0, 141)}</p>
+              <p>{item.description.slice(0, 141)}...</p>
             </div>
-            <a href={item.url} target="_blank" rel="noreferrer"><img className="resource-img" src={item.image} alt={""}/></a>
+            <div className="resource-img">
+              <a href={item.url} target="_blank" rel="noreferrer"><img src={item.image} alt={""}/></a>
+            </div>
           </div>
         )
       })}
